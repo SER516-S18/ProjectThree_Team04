9118d401c8fbce78895041766baead5e771462b5
saran1856 <skandim2@asu.edu>
2018-02-23 10:57:10 -0700

    added comments to TextConstants.java

--- src/team04/project2/constants/TextConstants.java
+++ src/team04/project2/constants/TextConstants.java
@@ -4,10 +4,14 @@ import java.awt.*;
 
 /**
  * TextConstants - Class containing program-wide constant Fonts and Strings
+ * @author David Henderson (dchende2@asu.edu)
+ * @version 1.0
+ * @since   2018-02-22 
  */
 
 public class TextConstants {
-    public static final Font LARGE_FONT = new Font("Monospaced", Font.PLAIN, 20);
+    
+	public static final Font LARGE_FONT = new Font("Monospaced", Font.PLAIN, 20);
     public static final Font DEFAULT_FONT = new Font("Monospaced", Font.PLAIN, 16);
     public static final Font SMALL_FONT = new Font("Monospaced", Font.PLAIN, 13);
 
