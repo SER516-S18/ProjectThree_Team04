9d63d4d3ba0541f45760e6194f3a745bd0c43a58
sahebjohar92 <sahebsingh10@gmail.com>
2018-04-10 19:35:54 -0700

    Changes

--- src/main/java/team04/project3/ui/client/ClientExpressionFaceView.java
+++ src/main/java/team04/project3/ui/client/ClientExpressionFaceView.java
@@ -10,6 +10,9 @@ import team04.project3.model.client.ClientModel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.QuadCurve2D;
+import java.awt.geom.Rectangle2D;
+import java.util.Vector;
 
 /**
  * Class to draw a face and represent the facial expressions.
@@ -54,8 +57,6 @@ public class ClientExpressionFaceView extends JPanel {
     /**
      * Draws the face with the required expressions
      * @param g Is a Graphics Object
-     * @param x Specifies the starting point on the x axis
-     * @param y Specifies the starting point on the y axis
      * @param height Specifies the height of the window
      * @param width Specifies the width of the window
      */
@@ -64,6 +65,9 @@ public class ClientExpressionFaceView extends JPanel {
         if (emostatePacket == null)
             emostatePacket = EmostatePacketBuilder.getZeroedEmostatePacket().build();
 
+        SCALE_X_FACTOR = width / 100.0;
+        SCALE_Y_FACTOR = height / 100.0;
+        
         String eyeDirection = "";
         boolean leftEyeBlink = false;
         boolean rightEyeBlink = false;
@@ -92,27 +96,13 @@ public class ClientExpressionFaceView extends JPanel {
         } else if (emostatePacket.getExpression(Expression.LOOK_RIGHT) > 0) {
             eyeDirection = Expression.LOOK_RIGHT.NAME;
         }
-
-        initialize(height, width);
+        
         renderFace(g);
-        renderEyes(g, leftEyeBlink, rightEyeBlink);
-        renderPupils(g, eyeDirection, leftEyeBlink, rightEyeBlink);
+        renderEyes(g, eyeDirection, leftEyeBlink, rightEyeBlink);
         renderEyeBrows(g, raiseBrowVal, furrowBrowVal);
         renderMouth(g, smile, clench, smirkLeft, smirkRight, laugh);
 
         this.repaint();
-    }
-
-    /**
-     * Finds the scaling factors so as to scale the face according to the background.
-     * @param x Specifies the starting point on the x axis
-     * @param y Specifies the starting point on the y axis
-     * @param height Specifies the height of the window
-     * @param width Specifies the width of the window
-     */
-    private void initialize(int height, int width) {
-        SCALE_X_FACTOR = width / 100.0;
-        SCALE_Y_FACTOR = height / 100.0;
     }
 
     /**
@@ -129,22 +119,22 @@ public class ClientExpressionFaceView extends JPanel {
     /**
      * Makes the eyes along with the various expressions needed
      */
-    private void renderEyes(Graphics g, boolean isLeftBlink, boolean isRightBlink) {
+    private void renderEyes(Graphics g, String eyeDirection, boolean isLeftBlink, boolean isRightBlink) {
         if (isLeftBlink && isRightBlink) {
-            createLine(g, DimensionConstants.LEFT_EYE_X_POSITION + DimensionConstants.EYE_RADIUS,  DimensionConstants.EYE_Y_POSITION,
+            renderLine(g, DimensionConstants.LEFT_EYE_X_POSITION + DimensionConstants.EYE_RADIUS,  DimensionConstants.EYE_Y_POSITION,
                     DimensionConstants.LEFT_EYE_X_POSITION - DimensionConstants.EYE_RADIUS,  DimensionConstants.EYE_Y_POSITION);
-            createLine(g, DimensionConstants.RIGHT_EYE_X_POSITION - DimensionConstants.EYE_RADIUS, DimensionConstants.EYE_Y_POSITION,
+            renderLine(g, DimensionConstants.RIGHT_EYE_X_POSITION - DimensionConstants.EYE_RADIUS, DimensionConstants.EYE_Y_POSITION,
                     DimensionConstants.RIGHT_EYE_X_POSITION + DimensionConstants.EYE_RADIUS, DimensionConstants.EYE_Y_POSITION);
         } else if (isLeftBlink) {
             g.setColor(Color.WHITE);
             createOval(g, DimensionConstants.LEFT_EYE_X_POSITION, DimensionConstants.EYE_Y_POSITION, DimensionConstants.EYE_RADIUS,
                     DimensionConstants.EYE_RADIUS);
             g.setColor(Color.BLACK);
-            createLine(g, DimensionConstants.RIGHT_EYE_X_POSITION - DimensionConstants.EYE_RADIUS,
+            renderLine(g, DimensionConstants.RIGHT_EYE_X_POSITION - DimensionConstants.EYE_RADIUS,
                     DimensionConstants.EYE_Y_POSITION, DimensionConstants.RIGHT_EYE_X_POSITION + DimensionConstants.EYE_RADIUS,
                     DimensionConstants.EYE_Y_POSITION);
         } else if (isRightBlink) {
-            createLine(g, DimensionConstants.LEFT_EYE_X_POSITION + DimensionConstants.EYE_RADIUS,
+            renderLine(g, DimensionConstants.LEFT_EYE_X_POSITION + DimensionConstants.EYE_RADIUS,
                     DimensionConstants.EYE_Y_POSITION, DimensionConstants.LEFT_EYE_X_POSITION - DimensionConstants.EYE_RADIUS,
                     DimensionConstants.EYE_Y_POSITION);
             g.setColor(Color.WHITE);
@@ -159,12 +149,14 @@ public class ClientExpressionFaceView extends JPanel {
                     DimensionConstants.EYE_RADIUS, DimensionConstants.EYE_RADIUS);
             g.setColor(Color.BLACK);
         }
+
+        renderPupils(g, eyeDirection, isLeftBlink, isRightBlink);
     }
 
     /**
      * Makes the pupils along with the various expressions needed.
      * @param g Is a Graphics Object
-     * @param eyeDirection Contains the direction values for looking left or right
+     * @param eyeDirection
      */
     private void renderPupils(Graphics g, String eyeDirection, boolean isBlinkLeft, boolean isBlinkRight) {
         int correctEYE = 2;
@@ -199,20 +191,20 @@ public class ClientExpressionFaceView extends JPanel {
      * Makes the eyebrows along with the various expressions needed.
      * @param g Is a Graphics Object
      */
-    private void renderEyeBrows(Graphics g, double p1, double p2) {
+    private void renderEyeBrows(Graphics g, double raiseBrowVal, double furrowBrowVal) {
         int y1, y2;
-        if (p1 != 0.0) {
-            y1 = DimensionConstants.EYE_BROW_Y_POSITION + (int) (p1 * 5);
-            y2 = DimensionConstants.EYE_BROW_Y_POSITION - (int) (p1 * 5);
-        } else if (p2 != 0.0) {
-            y1 = DimensionConstants.EYE_BROW_Y_POSITION - (int) (p2 * 5);
-            y2 = DimensionConstants.EYE_BROW_Y_POSITION + (int) (p2 * 5);
+        if (raiseBrowVal > 0.0) {
+            y1 = DimensionConstants.EYE_BROW_Y_POSITION + (int) (raiseBrowVal * SCALE_X_FACTOR);
+            y2 = DimensionConstants.EYE_BROW_Y_POSITION - (int) (raiseBrowVal * SCALE_X_FACTOR);
+        } else if (furrowBrowVal > 0.0) {
+            y1 = DimensionConstants.EYE_BROW_Y_POSITION - (int) (furrowBrowVal * SCALE_X_FACTOR);
+            y2 = DimensionConstants.EYE_BROW_Y_POSITION + (int) (furrowBrowVal * SCALE_X_FACTOR );
         } else {
             y1 = DimensionConstants.EYE_BROW_Y_POSITION;
             y2 = DimensionConstants.EYE_BROW_Y_POSITION;
         }
-        createLine(g, DimensionConstants.EYE_BROW_LEFT_X1, y1, DimensionConstants.EYE_BROW_LEFT_X2, y2);
-        createLine(g, DimensionConstants.EYE_BROW_RIGHT_X1, y2, DimensionConstants.EYE_BROW_RIGHT_X2, y1);
+        renderLine(g, DimensionConstants.EYE_BROW_LEFT_X1, y1, DimensionConstants.EYE_BROW_LEFT_X2, y2);
+        renderLine(g, DimensionConstants.EYE_BROW_RIGHT_X1, y2, DimensionConstants.EYE_BROW_RIGHT_X2, y1);
     }
 
     /**
@@ -220,7 +212,7 @@ public class ClientExpressionFaceView extends JPanel {
      * @param g Is a Graphics Object
      */
     public void renderNose(Graphics g) {
-        createLine(g, DimensionConstants.NOSE_X_POSITION, DimensionConstants.NOSE_Y1_POSITION, DimensionConstants.NOSE_X_POSITION ,
+        renderLine(g, DimensionConstants.NOSE_X_POSITION, DimensionConstants.NOSE_Y1_POSITION, DimensionConstants.NOSE_X_POSITION ,
                 DimensionConstants.NOSE_Y1_POSITION + DimensionConstants.NOSE_WIDTH);
     }
 
@@ -237,7 +229,7 @@ public class ClientExpressionFaceView extends JPanel {
         double y3 = DimensionConstants.MOUTH_Y_POSITION;
         double y4 = DimensionConstants.MOUTH_Y_POSITION;
 
-        double[] data = new double[7];
+        double[] data;
 
         if (smile > 0.0) {
             data = renderSmile(x1, x2, x3, y1, y2, y3, y4, smile);
@@ -259,12 +251,11 @@ public class ClientExpressionFaceView extends JPanel {
         g.setColor(Color.BLACK);
     }
 
-    private double[] renderClench(double x1, double x2,double x3, double y1, double y2, double y3, double y4, double clench) {
-
-        x1 = x1 - (clench * 5);
-        x2 = x2 + (clench * 5);
-        y3 = y3 - ((clench / 2.0) * 10);
-        y4 = y4 + ((clench / 2.0) * 10);
+    private double[] renderSmile(double x1, double x2,double x3, double y1, double y2, double y3, double y4, double smile) {
+        x1 = x1 - (smile * 5);
+        x2 = x2 + (smile * 5);
+        y3 = y3 + ((smile / 2.0) * 10);
+        y4 = y3;
 
         double[] data = new double[7];
         data[0] = x1;
@@ -278,11 +269,12 @@ public class ClientExpressionFaceView extends JPanel {
         return data;
     }
 
-    private double[] renderSmile(double x1, double x2,double x3, double y1, double y2, double y3, double y4, double smile) {
-        x1 = x1 - (smile * 5);
-        x2 = x2 + (smile * 5);
-        y3 = y3 + ((smile / 2.0) * 10);
-        y4 = y3;
+    private double[] renderClench(double x1, double x2,double x3, double y1, double y2, double y3, double y4, double clench) {
+
+        x1 = x1 - (clench * 5);
+        x2 = x2 + (clench * 5);
+        y3 = y3 - ((clench / 2.0) * 10);
+        y4 = y4 + ((clench / 2.0) * 10);
 
         double[] data = new double[7];
         data[0] = x1;
@@ -363,14 +355,7 @@ public class ClientExpressionFaceView extends JPanel {
 
 
     /**
-     * Used to make the lips
-     * @param g Is a Graphics Object
-     * @param x1 Contains the starting point of the lips on the horizontal plane
-     * @param y1 Contains the starting point of the lips on the vertical plane
-     * @param x2 Contains the ending point of the lips on the horizontal plane
-     * @param y2 Contains the ending point of the lips on the vertical plane
-     * @param x3 Contains the center point of the lips on the horizontal plane
-     * @param y3 Contains the center point of the lips on the vertical plane
+     * Make lips
      */
     private void renderLips(Graphics g, double x1, double y1, double x2, double y2, double x3, double y3) {
         int i, new_x, new_y, last_x, last_y;
@@ -389,7 +374,7 @@ public class ClientExpressionFaceView extends JPanel {
         for (i = (int) x1, last_x = (int) x1, last_y = (int) y1; i <= x2; i++) {
             new_x = i;
             new_y = (int) ((a * Math.pow(i, 2)) + (bb * i) + c);
-            createLine(g, last_x, last_y, new_x, new_y);
+            renderLine(g, last_x, last_y, new_x, new_y);
             last_x = new_x;
             last_y = new_y;
         }
@@ -397,70 +382,41 @@ public class ClientExpressionFaceView extends JPanel {
 
     /**
      * Creates a Circle
-     * @param g Is a Graphics Object
-     * @param x Contains the x position with respect to the background
-     * @param y Contains the y position with respect to the background
-     * @param radius Contains the radius of the circle to be drawn
      */
     private void createCircle(Graphics g, int x, int y, int radius) {
-        g.fillOval(scale_x(x - radius) + ORIGIN_X_COORD, scale_y(y - radius) + ORIGIN_Y_COORD, scale_x(radius * 3),
-                scale_y(radius * 2));
+        g.fillOval(xScale(x - radius) + ORIGIN_X_COORD, yScale(y - radius) + ORIGIN_Y_COORD, xScale(radius * 3),
+                yScale(radius * 2));
     }
 
     /**
      * Creates an Oval
-     * @param g Is a Graphics Object
-     * @param x Contains the x position with respect to the background
-     * @param y Contains the y position with respect to the background
-     * @param height Contains the height to the oval to be drawn
-     * @param width Contains the width of the oval to be drawn
      */
     private void createOval(Graphics g, int x, int y, int width, int height) {
-        g.fillOval(scale_x(x - width) + ORIGIN_X_COORD, scale_y(y - height) + ORIGIN_Y_COORD, scale_x(width * 3),
-                scale_y(height * 2));
+        g.fillOval(xScale(x - width) + ORIGIN_X_COORD, yScale(y - height) + ORIGIN_Y_COORD, xScale(width * 3),
+                yScale(height * 2));
     }
 
     /**
-     * Used to fill the hollow Oval
-     *
-     * @param g Is a Graphics Object
-     * @param x Contains the x position with respect to the background
-     * @param y Contains the y position with respect to the background
-     * @param height Contains the height till which the oval to be filled
-     * @param width Contains the width till which the oval to be filled
+     * Used to fill the Oval
      */
     private void fillOval(Graphics g, int x, int y, int height, int width) {
-        g.fillOval(scale_x(x - width) + ORIGIN_X_COORD, scale_y(y - height) + ORIGIN_Y_COORD, scale_x(width * 2),
-                scale_y(height * 2));
+        g.fillOval(xScale(x - width) + ORIGIN_X_COORD, yScale(y - height) + ORIGIN_Y_COORD, xScale(width * 2),
+                yScale(height * 2));
     }
 
     /**
      * Used to create a line
-     * @param g Is a Graphics Object
-     * @param x1 Contains the horizontal starting position of the line
-     * @param y1 Contains the vertical starting position of the line
-     * @param x2 Contains the horizontal ending position of the line
-     * @param y2 Contains the vertical ending position of the line
      */
-    private void createLine(Graphics g, int x1, int y1, int x2, int y2) {
-        g.drawLine(scale_x(x1) + ORIGIN_X_COORD, scale_y(y1) + ORIGIN_X_COORD, scale_x(x2) + ORIGIN_X_COORD, scale_y(y2) + ORIGIN_X_COORD);
+    private void renderLine(Graphics g, int x1, int y1, int x2, int y2) {
+        g.drawLine(xScale(x1) + ORIGIN_X_COORD, yScale(y1) + ORIGIN_X_COORD, xScale(x2) + ORIGIN_X_COORD, yScale(y2) + ORIGIN_X_COORD);
     }
 
-    /**
-     * Used to scale the input according to the horizontal factor of the background
-     * @param x Parameter needed to be scaled
-     * @return Scaled parameter
-     */
-    private int scale_x(int x) {
+    private int xScale(int x) {
         return (int) (x * SCALE_X_FACTOR);
     }
 
-    /**
-     * Used to scale the input according to the vertical factor of the background
-     * @param y Parameter needed to be scaled
-     * @return Scaled parameter
-     */
-    private int scale_y(int y) {
+
+    private int yScale(int y) {
         return (int) (y * SCALE_Y_FACTOR);
     }
 
